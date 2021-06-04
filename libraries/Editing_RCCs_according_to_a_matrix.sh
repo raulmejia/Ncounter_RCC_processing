8872110ce9172cc0c176e5b4dbbc647ee8876bad
raulmejia <r8a8m8p8@gmail.com>
2021-06-04 18:37:27 +0200

    RCCs eddited from matrix 1.0

--- libraries/Editing_RCCs_according_to_a_matrix.sh
+++ libraries/Editing_RCCs_according_to_a_matrix.sh
@@ -22,27 +22,63 @@ echo $code_path
 echo $label
 echo $output_folder
 
-### Generating the intermediate files 
+########
+## Program starts
+#######
+
+############
+### Building the ingredients
+############
+
+###############
+### Generating the intermediate files that contain the counts 
+###############
 Rscript $code_path/libraries/Matrix_splited_in_OnlyCounts_and_TargetNamesPlusCounts_Flies.R \
 	-m $input_matrix_path \
 	-o $output_folder
 
-#echo $output_folder/OnlyCountColumn/*
+##########
+## Creating the generic tail for all the files
+##########
+mkdir -p $output_folder/tail
 
+rm $output_folder/tail/GenericTail.txt
+touch $output_folder/tail/GenericTail.txt
+echo \</Code_Summary\> >> $output_folder/tail/GenericTail.txt
+echo \\n >> $output_folder/tail/GenericTail.txt
+echo \<Messages\> >> $output_folder/tail/GenericTail.txt
+echo \</Messages\> >> $output_folder/tail/GenericTail.txt
+echo \\n >> $output_folder/tail/GenericTail.txt
 
+###########
+### Generating the body and header for the new RCC files
+##########
 mkdir -p $output_folder/body
 mkdir -p $output_folder/header
+mkdir -p $output_folder/RCCs_with_Transformed_values
+
 # Extracting the "body" from the RCC files
 for sample in $input_path/*.RCC;do
     name=${sample%.RCC}
     #echo $(basename $name)
-    grep -E '>' $sample  | awk '{print $1 $2 $3}' > $output_folder/body/$( basename $name ).body123.csv
-    paste $output_folder/body/$( basename $name ).body123.csv  $output_folder/OnlyCountColumn/$( basename $name ).RCC.OnlyCountColumn >  $output_folder/body/$( basename $name ).body123_transformed4
+#    grep -E ',' $sample  | awk '{print $1 $2 $3}' > $output_folder/body/$( basename $name ).body123.csv
+  grep -E 'CodeClass|Endogenous|Positive|Negative|Housekeeping' $sample  | awk '{print $1 $2 $3}' > $output_folder/body/$( basename $name ).body123.csv
+  
+    
+    paste -d ',' $output_folder/body/$( basename $name ).body123.csv  $output_folder/OnlyCountColumn/$( basename $name ).RCC.OnlyCountColumn >  $output_folder/body/$( basename $name ).body123_transformed4  # here is the body
 
-    sed '1,/<Code_Summary>/!d' $sample >  $output_folder/header/$( basename $name ).header
+    sed '1,/<Code_Summary>/!d' $sample >  $output_folder/header/$( basename $name ).header # Generating the header 
+    
+    cat $output_folder/header/$( basename $name ).header \
+	$output_folder/body/$( basename $name ).body123_transformed4 \
+	$output_folder/tail/GenericTail.txt \
+	> $output_folder/RCCs_with_Transformed_values/$( basename $name ).$label.RCC  # appending all the elements (header, body and tail)
+	
+  
 done
 
 
+#CodeClass | Endogenous | Positive | Negative | Housekeeping 
 
 
 
